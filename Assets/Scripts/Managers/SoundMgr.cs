63a10af0a35b1ea2b5b81080f6723e0541dedcca
Julian Fliegler <julianfliegler@gmail.com>
2022-04-20 14:12:29 -0700

    UI

---
+++ Assets/Scripts/Managers/SoundMgr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundMgr : MonoBehaviour
+{
+    AudioSource audioData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
